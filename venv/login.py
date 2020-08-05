53a7a62ba93148e9111fc7cc5d58cc08c883b1f5
Anastasiya <infgai@yandex.ru>
2020-08-05 17:24:44 +0300

    recognize admin-teacher

--- venv/login.py
+++ venv/login.py
@@ -1,9 +1,12 @@
 import sys
 import pyqt5_tools
-from PyQt5 import uic
+from PyQt5 import uic,QtCore
 from PyQt5.QtWidgets import QWidget,QApplication,QDialog,QDesktopWidget
+import sqlite3
 
 class Login_window(QDialog):
+    adminSignal = QtCore.pyqtSignal()
+    teacherSignal = QtCore.pyqtSignal()
     def __init__(self,parent=None):
         # поскольку окло логина может вызываться из родительского окна, обязательно добавить аргумент parent
         super(Login_window,self).__init__()
@@ -15,7 +18,31 @@ class Login_window(QDialog):
         # self.login_button.rejected.connect(sys.exit) exit закрывает родительское окно???
         self.login_button.rejected.connect(self.exec)
     def on_click(self):
-        print('hhhhhh')
+        con = sqlite3.connect('db1.db')
+        cur = con.cursor()
+        result = cur.execute(
+            'SELECT login, password FROM user').fetchall()
+        print(result)
+        for record in result:
+            print(record)
+            print((self.luser.text(),self.lpassword.text()))
+            if record[0]==self.luser.text():
+                if record[1]==self.lpassword.text():
+                    print('ok')
+                    psw = cur.execute(
+                        'SELECT user.login,rights.admin FROM user,rights WHERE user.id=rights.id').fetchall()
+                    print(psw)
+                    dic=dict(psw)
+                    if dic[self.luser.text()]==1:#является админом
+                        self.adminSignal.emit()
+                    else:
+                        self.teacherSignal.emit()
+                else:
+                    print('1111')
+                break
+            else:
+                print('0000')
+        con.close()
     def center(self):
         qr = self.frameGeometry()
         cp = QDesktopWidget().availableGeometry().center()
